aed77ec47ef7bea08a474ce883bd692dea9bac7d
Asude <asudeyilmaz76@gmail.com>
2026-03-06 16:02:30 +0300

    Update 58- csa- all.sql

--- sqludemy/58- csa- all.sql
+++ sqludemy/58- csa- all.sql
@@ -38,4 +38,35 @@ WHERE salary >
 		SELECT MAX(salary)
 		FROM employees
 		WHERE manager_id = 1
+
 	)
+
+	
+SELECT film_id, title, length
+FROM film
+WHERE
+	length <
+	(
+		select MIN((avg_length)) FROM
+		(
+			SELECT ROUND(AVG(length),2) avg_length
+			FROM film
+			GROUP BY rating
+		) sb
+	
+	) 
+order by 3 desc;
+
+
+--ALL İLE
+SELECT film_id, title, length
+FROM film
+WHERE
+	length < ALL			
+		(
+			SELECT ROUND(AVG(length),2) 
+			FROM film
+			GROUP BY rating
+		) 
+	
+order by 3 desc;
