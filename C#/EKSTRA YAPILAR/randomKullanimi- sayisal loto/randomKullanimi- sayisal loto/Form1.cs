62b37eaa7bbd69dd17d8669ad6ee83e6987234bf
Asude <asudeyilmaz76@gmail.com>
2026-05-11 17:13:19 +0300

    random kullanımı sınıf içinde

---
+++ C#/EKSTRA YAPILAR/randomKullanimi- sayisal loto/randomKullanimi- sayisal loto/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomKullanimi__sayisal_loto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rast = new Random();
+            int s1, s2, s3, s4;
+            s1 = rast.Next(1,5);
+            s2 = rast.Next(1,5);
+            s3 = rast.Next(1,5);
+            s4 = rast.Next(1,5);
+
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();    
+            label3.Text = s3.ToString();
+            label4.Text = s4.ToString();
+
+            karsilastirma krs = new karsilastirma(label1.Text, textBox1.Text, textBox1);
+            karsilastirma krs2 = new karsilastirma(label2.Text , textBox2.Text , textBox2 );
+            karsilastirma krs3 = new karsilastirma(label3.Text, textBox3.Text, textBox3);
+            karsilastirma krs4 = new karsilastirma(label4.Text, textBox4.Text, textBox4);
+
+        }
+    }
+}
---
+++ C#/EKSTRA YAPILAR/randomKullanimi- sayisal loto/randomKullanimi- sayisal loto/karsilastirma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomKullanimi__sayisal_loto
+{
+    internal class karsilastirma
+    {
+        public  karsilastirma(string labelText, string textBoxText, TextBox txt) 
+        {
+            
+            if (labelText == textBoxText)
+            {
+                txt.BackColor = Color.Green;
+            }else
+            {
+                txt.BackColor = Color.Red;
+            }
+        }
+    }
+}
